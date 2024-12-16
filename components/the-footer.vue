a81ae033f3611eca953f898cd1b8acc6d169d4a2
Alessandro <propriedade0@gmail.com>
2024-12-16 10:58:13 +0100

    creating_mobile_version

--- components/the-footer.vue
+++ components/the-footer.vue
@@ -2,12 +2,14 @@
   <footer
     class="pt-24 pb-16 bg-[#222831] font-medium from-sky-950 to-[#222831] bg-gradient-to-r text-white gap-6 grid grid-cols-12 to-25% w-full"
   >
-    <div class="capitalize col-start-2 col-end-12 flex justify-between">
-      <div class="relative">
+    <div
+      class="text-justify space-y-6 sm:space-y-0 flex flex-col items-center capitalize col-start-1 col-span-full sm:flex-row sm:col-start-2 sm:col-end-12 sm:justify-between"
+    >
+      <div class="relative flex flex-col items-center">
         <img
           src="/assets/about-pizza-img-1.png"
           alt="pizza-de-queijo"
-          class="absolute -top-[11.5rem] -left-28 w-48 h-48"
+          class="hidden sm:absolute sm:inline-block sm:-top-[11.5rem] sm:-left-28 w-48 h-48"
         />
         <Subtitle title="contacte-nos" class="mb-2" />
         <ul>
@@ -16,20 +18,26 @@
           <li class="lowercase">propriedade0@gmail.com</li>
         </ul>
       </div>
-      <div class="max-w-60">
+      <div class="flex flex-col items-center sm:max-w-60">
         <Subtitle title="vita restaurantes" class="mb-2" />
-        <p>Entrega rápida e reservas fáceis para uma experiência saborosa!</p>
+        <p class="max-w-60">
+          Entrega rápida e reservas fáceis para uma experiência saborosa!
+        </p>
       </div>
-      <div>
+      <div class="flex flex-col items-center">
         <Subtitle title="aberto" class="mb-2" />
         <p>todos os dias</p>
         <p>10:00H - 22:00H</p>
       </div>
     </div>
-    <p class="row-start-2 col-start-2 col-span-4">
+    <p
+      class="row-start-2 col-start-2 sm:col-start-2 col-span-10 text-center sm:text-left sm:col-span-4"
+    >
       &copy; 2024-2025 Vitasoft. Todos os direitos reservados
     </p>
-    <ul class="row-start-2 flex space-x-4 col-start-10 fa-lg">
+    <ul
+      class="row-start-3 sm:row-start-2 flex space-x-4 col-start-5 col-span-5 sm:col-start-10 fa-lg"
+    >
       <li>
         <NuxtLink to="/">
           <font-awesome :icon="faFacebookSquare" class="text-blue-700" />
--- components/the-header.vue
+++ components/the-header.vue
@@ -1,55 +1,70 @@
 <template>
   <header
-    class="grid grid-cols-12 items-center w-full from-[#222831] from-70% to-emerald-900 relative z-50 bg-gradient-to-r py-6 uppercase text-white font-medium max-h-36"
+    class="grid grid-cols-12 items-center w-full from-[#222831] from-70% to-emerald-900 relative z-50 bg-gradient-to-r py-6 uppercase text-white font-medium sm:max-h-36"
   >
-    <nav class="flex justify-between col-start-2 items-center col-end-12">
+    <nav
+      class="flex flex-col sm:flex-row col-start-2 col-span-10 items-start justify-between sm:col-start-2 sm:items-center sm:col-end-12"
+    >
       <NuxtLink to="/" class="flex items-center relative">
         <img
           src="/pizza-icon.png"
           alt="pizza-slice"
-          class="absolute h-20 w-20 -left-8"
+          class="absolute hidden sm:inline-block h-20 w-20 -left-8"
         />
-        <Subtitle title="vita restaurantes" class="translate-x-12" />
+        <Subtitle title="vita restaurantes" class="sm:translate-x-12" />
       </NuxtLink>
-      <ul class="flex space-x-4 items-center">
-        <NavLink class="hover:text-yellow-500 duration-300 transition-all">
-          <NuxtLink
-            to="/"
-            active-class="bg-gradient-to-r from-yellow-400 to-orange-400 bg-clip-text text-transparent"
-            >home</NuxtLink
-          >
-        </NavLink>
-        <NavLink class="hover:text-yellow-500 duration-300 transition-all">
-          <NuxtLink
-            to="/about"
-            active-class="bg-gradient-to-r from-yellow-400 to-orange-400 bg-clip-text text-transparent"
-            >sobre</NuxtLink
-          >
-        </NavLink>
-        <NavLink class="hover:text-yellow-500 duration-300 transition-all">
-          <NuxtLink
-            to="/menu"
-            active-class="bg-gradient-to-r from-yellow-400 to-orange-400 bg-clip-text text-transparent"
-            >menu</NuxtLink
-          >
-        </NavLink>
-        <NavLink class="hover:text-yellow-500 duration-300 transition-all">
-          <NuxtLink
-            to="/book-table"
-            active-class="bg-gradient-to-r from-yellow-400 to-orange-400 bg-clip-text text-transparent"
-            >fazer-reserva</NuxtLink
-          >
-        </NavLink>
-        <NavLink class="hover:text-yellow-500 duration-300 transition-all">
-          <NuxtLink
-            to="/testimonials"
-            active-class="bg-gradient-to-r from-yellow-400 to-orange-400 bg-clip-text text-transparent"
-            >depoimentos</NuxtLink
-          >
-        </NavLink>
-      </ul>
-      <div class="flex space-x-4 items-center">
-        <ul class="flex space-x-4">
+
+      <button
+        class="sm:hidden hover:text-yellow-500 duration-200 transition-all self-end text-lg -translate-y-6"
+        @click="setHidden"
+      >
+        <FontAwesome :icon="faList" />
+      </button>
+
+      <TransitionExpand>
+        <ul
+          class="sm:flex sm:space-x-4 items-center -translate-y-4 sm:-translate-y-0"
+          v-if="hidden"
+        >
+          <NavLink class="hover:text-yellow-500 duration-300 transition-all">
+            <NuxtLink
+              to="/"
+              active-class="bg-gradient-to-r from-yellow-400 to-orange-400 bg-clip-text text-transparent"
+              >home</NuxtLink
+            >
+          </NavLink>
+          <NavLink class="hover:text-yellow-500 duration-300 transition-all">
+            <NuxtLink
+              to="/about"
+              active-class="bg-gradient-to-r from-yellow-400 to-orange-400 bg-clip-text text-transparent"
+              >sobre</NuxtLink
+            >
+          </NavLink>
+          <NavLink class="hover:text-yellow-500 duration-300 transition-all">
+            <NuxtLink
+              to="/menu"
+              active-class="bg-gradient-to-r from-yellow-400 to-orange-400 bg-clip-text text-transparent"
+              >menu</NuxtLink
+            >
+          </NavLink>
+          <NavLink class="hover:text-yellow-500 duration-300 transition-all">
+            <NuxtLink
+              to="/book-table"
+              active-class="bg-gradient-to-r from-yellow-400 to-orange-400 bg-clip-text text-transparent"
+              >fazer-reserva</NuxtLink
+            >
+          </NavLink>
+          <NavLink class="hover:text-yellow-500 duration-300 transition-all">
+            <NuxtLink
+              to="/testimonials"
+              active-class="bg-gradient-to-r from-yellow-400 to-orange-400 bg-clip-text text-transparent"
+              >depoimentos</NuxtLink
+            >
+          </NavLink>
+        </ul>
+      </TransitionExpand>
+      <div class="flex sm:space-x-4 items-center">
+        <ul class="sm:flex space-x-4 hidden">
           <NavLink>
             <NuxtLink to="#"> <FontAwesome :icon="faUser" /></NuxtLink>
           </NavLink>
@@ -60,7 +75,11 @@
             <NuxtLink to="#"><FontAwesome :icon="faSearch" /></NuxtLink>
           </NavLink>
         </ul>
-        <BtnInline to="/order-online" link class="text-sm">
+        <BtnInline
+          to="/order-online"
+          link
+          class="text-sm hidden sm:inline-block"
+        >
           fazer pedido
         </BtnInline>
       </div>
@@ -73,5 +92,14 @@ import {
   faUser,
   faShoppingCart,
   faSearch,
+  faList,
 } from "@fortawesome/free-solid-svg-icons";
+
+import { TransitionExpand } from "@morev/vue-transitions";
+const hidden = ref(false);
+
+function setHidden() {
+  hidden.value = !hidden.value;
+  console.log(hidden.value);
+}
 </script>
